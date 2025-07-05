4f91cf6e6e72f8f205ab3bd9a4118ce2c00a2c0b
sqezze1 <mcray01@mail.ru>
2025-07-05 03:30:28 +0300

    удалил App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.tsx";
-import Lobby from "./pages/Lobby.tsx";
+import Home from "./pages/Home.tsx"; // Главная страница
+import Lobby from "./pages/Lobby.tsx"; // Страница лобби
+import Scenario from "./pages/Scenario.tsx"; // Страница сценария
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} /> {/* Главная страница */}
         <Route path="/lobby" element={<Lobby />} />
+        <Route path="/scenario/:roomId" element={<Scenario />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
